8fcd5a1f94121f6349fe99b65684a0a12eef1d9b
Richard.Wang <wangleihd@gmail.com>
2018-11-11 11:19:01 +0800

    Fixed mongodb connect error.

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	port: 8001,
-	url: 'mongodb:/cai:admin2010@ds159293.mlab.com:59293/minicai',
+	url: 'mongodb://cai:admin2010@ds159293.mlab.com:59293/minicai',
 	session: {
 		name: 'SID',
 		secret: 'SID',
